5cd1285ea723718b529d0fa93b01f2977dfa8042
Beka Karkusashvili <beqa.qarqusashvili.20@gmail.com>
2020-04-28 13:53:50 +0400

    fix(https): Fix speling

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (app()->environment('prodaction')) {
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 }
